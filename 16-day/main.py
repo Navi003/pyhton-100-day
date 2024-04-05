c60b6c32f7b21a35016be5c0ae2213de20193e5f
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-04-05 16:34:06 +0200

    added pokemon table

--- 16-day/main.py
+++ 16-day/main.py
@@ -1,14 +1,31 @@
 
-import turtle
+# import turtle
 
-from turtle import Turtle, Screen
-timmy = Turtle()
+# from turtle import Turtle, Screen
+# timmy = Turtle()
  
  
-print(timmy)
+# print(timmy)
 
-my_screen = Screen()
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+# my_screen = Screen()
 
 
-print(my_screen)
+# print(my_screen)
 
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+print(table)
+
+table.add_column("pokemon",["Pickachu","Squirtle","charmander"])
+table.add_column("type",["Electric","Water","Fire"])
+
+table.align  = "l"
+
+print(table)
